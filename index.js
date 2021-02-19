674f83ea59e30b717b855721a06662dcbf1d2938
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-18 17:41:07 -0800

    Completed Asssignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "NICK is the champion";
+document.body.appendChild(newHeader);
