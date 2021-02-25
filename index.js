742c11e1007a3338bdcbc3a9da24dee46ab76d2f
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-25 10:45:41 -0800

    commit tree 7b990979dff90166227dfc4d01432f0ac9a1e1c7

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ document.getElementById('main').remove();
 var newHeader = document.createElement('h1');
 newHeader.setAttribute("id","victory");
 newHeader.innerHTML = "NICK is the champion";
+
 document.body.appendChild(newHeader);
